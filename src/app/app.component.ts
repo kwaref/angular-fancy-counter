e627da7ac603d2527068c9244150cc0395cf92bf
Karel R. Varona <ex143449@edp.pt>
2024-04-27 21:48:05 +0100

    conditional css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from './card/card.component';
+import { CommonModule } from '@angular/common';
+import { CounterService } from './counter.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent],
+  imports: [RouterOutlet, CardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor(private counterService: CounterService) { }
   title = 'fancy-counter';
+  locked() {
+    return this.counterService.isLocked();
+  }
 }
